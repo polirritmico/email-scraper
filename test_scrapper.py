6ebb1bd89afd09514d9b160e679c759a1ecb94e1
polirritmico <ebra187@gmail.com>
2021-11-29 14:22:21 -0300

    test_scrapper.py added get file and get mail uc01 tests

--- test_scrapper.py
+++ test_scrapper.py
@@ -6,22 +6,33 @@ import os
 # import classes from the script
 from src.page import Page
 
-class TestInputs(unottest.TestCase):
-    def setUp(self):
-        self.html_collection = []
-        for filename in sorted(os.listdir("testFiles/")):
-            file = open(("testFiles/" + filename), "r")
-            html = file.read()
-            file.close()
-            page = Page("testFiles/" + filename)
-            self.html_collection.append(page)
+class TestInputs(unittest.TestCase):
+    #def setUp(self):
+    #    self.html_collection = []
+    #    for filename in sorted(os.listdir("testFiles/")):
+    #        file = open(("testFiles/" + filename), "r")
+    #        html = file.read()
+    #        file.close()
+    #        page = Page("testFiles/" + filename)
+    #        self.html_collection.append(page)
 
-    def test_uc01(self):
+    def test_get_file(self):
+        url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
+        page = Page(url)
+        out = page.readHTML()
+
+        self.assertIsNotNone(out)
+
+    def test_get_mail_uc01(self):
         url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
         expected = "alyonp@uc.cl"
-        for page in html_collection:
-            if page.filename != url:
-                continue
-            mail = page.getMail()
-            self.assert_equal(expected, mail)
 
+        page = Page(url)
+        page.readHTML()
+        out = page.getMails()
+
+        self.assertEqual(expected, out)
+
+
+if __name__ == "__main__":
+    unittest.main()
