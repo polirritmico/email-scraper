a69c30490983c9aa944474556082b44c72194b68
polirritmico <ebra187@gmail.com>
2021-11-29 16:16:25 -0300

    added pages collection class

--- email-scrapper.py
+++ email-scrapper.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from src.page import Page
 
-def main():
-    print("Running...")
+class PagesCollection:
+    def __init__(self, _file):
+        # Read the list file
+        self.list_file = ""
+        with open (_file, "r") as file:
+            self.list_file = file.read()
+
+def main(argv):
+    print("Leyendo lista de páginas en archivo: ")
+    print(str(sys.argv[1]))
+    #read the file
+    pages_collection = PagesCollection(sys.argv[1])
+    
+    #make the collection
+    
+    #process each
+
+
+
+    print("Lista de páginas leída")
+    print(pages_collection.list_file)
+    print("Procesando las páginas:")
+
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
