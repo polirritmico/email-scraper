2001729ea3d63f5e6b9488e660b68fb4ff5053e4
polirritmico <ebra187@gmail.com>
2021-11-29 11:45:58 -0300

    base commit

---
+++ src/scrapper.py
@@ -0,0 +1,18 @@
+import re
+from selenium import webdriver
+
+
+#EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+
+url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
+
+#content = get(url).content
+#soup = BeautifullSoup(content, "lxml")
+
+option = webdriver.ChromeOptions()
+option.add_argument("--headless")
+browser = webdriver.Chrome(executable_path="./chromedriver", options=option)
+
+browser.get(url)
+
+
