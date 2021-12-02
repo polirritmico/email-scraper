bda36db91170df9e6aec08f4a84c80dd248ea028
polirritmico <ebra187@gmail.com>
2021-12-02 12:48:51 -0300

    Added empty line and verbose behaviour

--- src/pagesCollection.py
+++ src/pagesCollection.py
@@ -4,6 +4,8 @@
 import time
 from src.page import Page
 
+SEP = "----------------------------------\n"
+
 class PagesCollection:
     def __init__(self, _file):
         self.list_raw = ""
@@ -14,21 +16,30 @@ class PagesCollection:
             with open (_file, "r") as file:
                 self.list_raw = file.read()
             for line in self.list_raw.splitlines():
-                if line.strip() == "":
-                    continue
+                # if line.strip() == "":
+                #     line = "none"
                 self.url_list.append(line)
         except:
             print("Error leyendo el archivo")
             return -1
 
-    def scrapUrlList(self, delay = 0.125):
+    def scrapUrlList(self, delay = 0.125, verbose = False):
+        if verbose: print(SEP + "Regex matches:")
         for url in self.url_list:
+            if url == "":
+                self.collected_data.append("")
+                if verbose: print("")
+                continue
+
             page = Page(url)
             page.readHTML()
             page.processHTML()
             self.collected_data.append(page.getMatchData())
+
+            if verbose: print(page.getMatchData())
             # Add a delay to avoid bans
             time.sleep(delay)
+        if verbose: print(SEP)
 
     def getDataList(self):
         out_string = ""
