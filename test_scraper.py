93e652280e1039dd9b5986aa50b6867cc8b85151
polirritmico <ebra187@gmail.com>
2021-12-03 00:02:54 -0300

    Splited test to different test classes

--- test_scraper.py
+++ test_scraper.py
@@ -9,6 +9,7 @@ from src.page import Page
 from src.pagesCollection import PagesCollection
 from src.browser import Browser
 
+#@unittest.skip
 class TestInputs(unittest.TestCase):
     def test_get_web_file(self):
         url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
@@ -29,6 +30,7 @@ class TestInputs(unittest.TestCase):
 
         self.assertEqual(expected, out)
 
+class TestMails(unittest.TestCase):
     def test_get_mail_uc01(self):
         url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
         expected = "alyonp@uc.cl"
@@ -64,15 +66,6 @@ class TestInputs(unittest.TestCase):
 
         self.assertEqual(expected, out)
 
-    def test_get_page_url_list(self):
-        file = "testFiles/simple_list.txt"
-        expected="""http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/2201-vergara-blanco-alejandro\nhttp://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/459-fermandois-vohringer-arturo"""
-
-        collection = PagesCollection(file)
-        out = "\n".join(collection.url_list)
-
-        self.assertEqual(expected, out)
-
     def test_simple_web_scrap(self):
         file = "testFiles/simple_list.txt"
         expected = """alejandro.vergara@uc.cl\naferman@uc.cl"""
@@ -80,6 +73,16 @@ class TestInputs(unittest.TestCase):
         collection = PagesCollection(file)
         collection.scrapUrlList()
         out = collection.getDataList()
+
+        self.assertEqual(expected, out)
+
+class TestList(unittest.TestCase):
+    def test_get_page_url_list(self):
+        file = "testFiles/simple_list.txt"
+        expected="""http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/2201-vergara-blanco-alejandro\nhttp://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/459-fermandois-vohringer-arturo"""
+
+        collection = PagesCollection(file)
+        out = "\n".join(collection.url_list)
 
         self.assertEqual(expected, out)
 
