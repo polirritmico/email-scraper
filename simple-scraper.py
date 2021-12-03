7af190c686d7d0e65045167ae683df2213586504
polirritmico <ebra187@gmail.com>
2021-12-02 22:26:02 -0300

    Improve all outputs, fix inputs args and improve code in general

--- simple-scraper.py
+++ simple-scraper.py
@@ -3,34 +3,42 @@
 
 import sys
 import getopt
-from src.pagesCollection import PagesCollection
+from src.pagesCollection import PagesCollection, SEP
 from src.page import Page
 from src.browser import Browser
 
+def launchBrowser(page):
+    browser = Browser()
+    page.readHTML_JS(browser)
+    browser.quit()
 
-def scrapUrl(verbose, javascript, url):
+def scrapUrl(url, verbose, javascript):
+    if verbose: print(SEP + "RegEx matches:\n" + SEP)
+
     page = Page(url)
     if javascript:
-        page.readFile()
+        launchBrowser(page)
     else:
-        page.readHTML_JS(Browser())
+        page.readFile()
     page.processHTML()
     if verbose: print(page.getMatchData())
 
+    if verbose: print(SEP)
+
     return page.getMatchData()
 
-def scrapFile(verbose, javascript, input_file):
+def scrapFile(input_file, verbose, javascript):
     page = Page(input_file)
     if javascript:
-        page.readFile()
+        launchBrowser(page)
     else:
-        page.readHTML_JS(Browser())
+        page.readFile()
     page.processHTML()
     if verbose: print(page.getMatchData())
 
     return page.getMatchData()
 
-def scrapListUrl(verbose, delay, javascript, input_file):
+def scrapListUrl(input_file, verbose, javascript, delay):
     collection = PagesCollection(input_file, verbose, delay, javascript)
     collection.scrapUrlList()
 
@@ -41,16 +49,31 @@ def writeOutFile(out_data, outfile):
         file.write(out_data)
 
 def usage():
-    print("Utilice -l <file.txt> para procesar un archivo de lista\nUtilice -u <url> para procesar una lista.")
+    print("""
+Uso: simple-scraper.py <Opciones> <Modo> <Archivo/URL> <Salida>
+Busca en cada página y devuelve los resultados en un archivo de texto.
+
+Opciones:
+   -h, --help        Muestra este texto y cierra el programa.
+   -v, --verbose     Para activar la salida en consola.
+   -d, --delay       Para añadir un tiempo en segundos de espera entre
+                     cada url. No funciona en el modo JavaScript.
+   -j, --javascript  Obtiene el HTML a través de un "navegador virtual".
+
+Modo:
+   -l, --list        Para procesar un archivo de lista.
+   -u, --url         Para procesar una lista.
+   -f, --file        Para procesar un HTML descargado.
+
+Ejemplo de uso:
+./simple-scraper.py -vj -d 2 --url http://www.pagina.com salida.txt
+""")
 
 
 def main(argv):
     # Check input parameters
     try:
-        opts, args = getopt.getopt( sys.argv[1:], "vd:jluf",       \
-                                    [ "--verbose"    , "--delay="  \
-                                    , "--javascript" , "--list"    \
-                                    , "--url"        , "--file" ])
+        opts, args = getopt.getopt( sys.argv[1:] , "hvjd:luf", ["help", "verbose", "javascript", "delay=" , "list" , "url", "file"])
     except getopt.GetoptError:
         print("Simple-scraper: Opción inválida.")
         usage()
@@ -64,7 +87,8 @@ def main(argv):
     out_data    = ""
 
     # Setting input/output
-    try: in_file = args[0]
+    try:
+        in_file = args[0]
     except:
         print("Simple-scraper: Falta archivo o url objetivo.")
         return 2
@@ -77,19 +101,21 @@ def main(argv):
 
     for opt, arg in opts:
         # Setting up the options
-        if opt in ("-v", "--verbose"):
+        if opt in ("-h", "--help"):
+            usage()
+        elif opt in ("-v", "--verbose"):
             verbose = True
-        elif opt in ("-d", "--delay"):
-            delay = float(arg)
         elif opt in ("-j", "--javascript"):
             javascript = True
+        elif opt in ("-d", "--delay"):
+            delay = float(arg)
 
         # Run the proper mode
         elif opt in ("-l", "--list"):
-            out_data = scrapList(in_file, verbose, delay, javascript)
+            out_data = scrapListUrl(in_file, verbose, javascript, delay)
             break
         elif opt in ("-u", "--url"):
-            out_data = scrapURL(in_file, verbose, javascript)
+            out_data = scrapUrl(in_file, verbose, javascript)
             break
         elif opt in ("-f", "--file"):
             out_data = scrapFile(in_file, verbose, javascript)
@@ -104,13 +130,13 @@ def main(argv):
         print("ERROR: No se procesaron datos.")
         return -1
     
-    print("Procesado OK\nEscribiendo archivo {0}".format(out_file))
+    print("Procesado OK\nEscribiendo archivo {0}...".format(out_file))
     try:
         writeOutFile(out_data, out_file)
     except:
         print("ERROR: No se pudo escribir el archivo.")
         return -1
-    print("Archivo de salida escrito correctamente.")
+    print("Archivo escrito correctamente.")
 
     return 0
 
