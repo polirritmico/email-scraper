8edd7bf771f336ac5dfea195a61c4b25ed08a5ba
polirritmico <ebra187@gmail.com>
2021-12-03 04:21:35 -0300

    added uchile ug

--- config.py
+++ config.py
@@ -34,6 +34,9 @@ GECKO_LOG_PATH = r"WebDriver/geckodriver.log"
 # Template
 #SEARCH = r""""""
 
+# This should download the page:
+#SEARCH = r""".*"""
+
 # Email search
 # Taken from https://emailregex.com/
 #SEARCH = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
@@ -47,5 +50,10 @@ GECKO_LOG_PATH = r"WebDriver/geckodriver.log"
 # Doctorado
 #SEARCH = r"""li>(.*?)((Doctora?)|([Ph]\.?[Hh]\.?[Dd]\.?)) (.*?)</li"""
 
-# Licenciatura
-SEARCH = r""">(.*?)Licenciad[oa] (.+?)<"""
+# Licenciatura UC
+#SEARCH = r""">(.*?)Licenciad[oa] (.+?)<"""
+# Licenciatura UCHILE
+#SEARCH = r"""<tr class=\"even\"><td class=\"\">ABOGAD[OA]<\/td><td class=\"\">(.*?)<\/td><td class=\"\">(.*?)<\/td><\/tr>
+SEARCH = r"""<tr class=\"even\"><td class=\"\">ABOGAD[OA]</td><td class=\"\">(.*?)</td><td class=\"\">(.*?)</td></tr>"""
+
+
