4d660c42ea063be87542d8c5604e8068a0eded15
polirritmico <ebra187@gmail.com>
2021-12-08 16:39:37 -0300

    Group all base test

--- unittest_scraper.py
+++ unittest_scraper.py
@@ -12,8 +12,8 @@ from src.browser import Browser
 REGEX_MAIL   = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 REGEX_MASTER = r"""<li>(.*?)(M[aá](g[ií])?ster)(.*?)</li>"""
 
-#@unittest.skip
-class TestInputs(unittest.TestCase):
+@unittest.skip
+class TestBase(unittest.TestCase):
     def test_get_web_file(self):
         url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
         page = Page(url, REGEX_MAIL)
@@ -33,8 +33,6 @@ class TestInputs(unittest.TestCase):
 
         self.assertEqual(expected, out)
 
-#@unittest.skip
-class TestMails(unittest.TestCase):
     def test_get_mail_uc01(self):
         url = "http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-privado/581-lyon-puelma-alberto"
         expected = "alyonp@uc.cl"
@@ -80,8 +78,6 @@ class TestMails(unittest.TestCase):
 
         self.assertEqual(expected, out)
 
-#@unittest.skip
-class TestList(unittest.TestCase):
     def test_get_page_url_list(self):
         file = "test_files/simple_list.txt"
         expected="""http://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/2201-vergara-blanco-alejandro\nhttp://derecho.uc.cl/es/profesores/nomina-por-departamento/departamento-de-derecho-publico/459-fermandois-vohringer-arturo"""
@@ -111,8 +107,6 @@ class TestList(unittest.TestCase):
 
         self.assertEqual(expected, out)
 
-#@unittest.skip
-class TestSearches(unittest.TestCase):
     def test_get_magister_uandes(self):
         url = "https://www.uandes.cl/personas/manuel-bernet-paez/"
         expected = "<li>Máster en Derecho Privado, Ilustre Colegio de Abogados de Madrid, España.</li>"
@@ -123,6 +117,11 @@ class TestSearches(unittest.TestCase):
         out = page.getMatchData()
 
         self.assertEqual(expected, out)
+
+#@unittest.skip
+class TestNewFunction(unittest.TestCase):
+    def test_new_function(self):
+        pass
 
 
 if __name__ == "__main__":
