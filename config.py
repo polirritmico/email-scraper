535ffc61d19a99ec7ec2ec93beec0446cf93954a
polirritmico <ebra187@gmail.com>
2021-12-09 16:19:57 -0300

    Added search patterns

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ SEARCH = r""
 # Buscar correos sin = ni /
 #SEARCH = r"""(?:[a-z0-9!#$%&'*+?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
-# UG
+# Pregrados
 #SEARCH = r"""([Aa]bogad[oa])|(ABOGAD[OA])|(Licenciad[oa])"""
 
 # Master
@@ -102,7 +102,7 @@ SEARCH = r""
 # Doctorados
 #SEARCH = r""">(.*?)((Doctora?)|([Ph]\.?[Hh]\.?[Dd]\.?)) (.*?)</"""
 
-# UG
+# Pregrados
 #SEARCH = r"""<a class=\"card persona\" href=\"(.*?)\">(.*?)</div>"""
 
 # ####################################
@@ -114,11 +114,17 @@ SEARCH = r""
 # Master
 #SEARCH = r"""<li(.*?)>(.*?)(M[aá](g[ií])?ster)(.*?)[ \n\t]*?</li>"""
 
-# UG
+# Pregrados
 #SEARCH = r"""<li(.*?)>(.*?)([Aa]bogad[oa])|(ABOGAD[OA])|(Licenciad[oa])(.*?)</li>"""
 
 # ####################################
 # PUCV
 
 # Doctorados
-SEARCH = r""">(.*?)((Doctora?)|([Ph]\.?[Hh]\.?[Dd]\.?)) (.*?)</"""
+#SEARCH = r""">(.*?)((Doctora?)|([Ph]\.?[Hh]\.?[Dd]\.?)) (.*?)</"""
+
+# Master
+#SEARCH = r""">(M[aá](g[ií])?ster)(.*?)[ \n\t]*?</"""
+
+# Pregrados
+SEARCH = r""">(([Aa]bogad[oa])|(ABOGAD[OA])|(Licenciad[oa]))(.+?)<"""
